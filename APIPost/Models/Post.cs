50b085be5fafb12e4207ae0cd78142a065d793e7
Biggy <jululien.gautier@laposte.net>
2020-01-16 08:41:10 +0100

    Ajoutez des fichiers projet.

---
+++ APIPost/Models/Post.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIPost.Models
+{
+    public class Post
+    {
+        public Guid MessageId { get; set; }
+        public Guid UserId { get; set; }
+        prop
+    }
+}
